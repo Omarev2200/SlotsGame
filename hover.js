54156b7edf93d723f06824a671324f7be4275c8c
Stanislav <stanislav.omarev@gmal.com>
2021-11-04 13:00:01 +0200

    refactoring

--- hover.js
+++
@@ -1,7 +0,0 @@
-export function hover() {
-    $('.card-game').on('mouseenter mouseleave', function (ev) {
-        let $element = this.childNodes[3];
-        if (ev.type === "mouseenter") { $($element).css('opacity', '0'); }
-        else if (ev.type === "mouseleave") { $($element).css('opacity', '1'); }
-      })
-}
